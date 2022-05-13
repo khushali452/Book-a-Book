b22f29716d77b206a3bee08ea428c5d32f43b9d3
khushali452 <khushalivasani.ict19@gmail.com>
2022-05-13 23:30:15 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100vh;
+    backgraound-color: red;
+`
+
+const App = () => {
+  return (
+    <div><Container>Hello World</Container></div>
+  )
+};
+
+export default App;
