bfe639bd393f4a05fd025659d2b2e98094801988
khushali452 <khushalivasani.ict19@gmail.com>
2022-05-16 11:09:56 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Navbar from './components/Navbar';
 
 const Container = styled.div`
     height: 100vh;
-    backgraound-color: red;
+    
 `
 
 const App = () => {
   return (
-    <div><Container>Hello World</Container></div>
+    <div><Container>
+      <Navbar/>
+      </Container></div>
   )
 };
 
