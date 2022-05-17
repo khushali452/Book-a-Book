7d1efcaaa66281d6cde9172f1b6813a409770dc5
khushali452 <khushalivasani.ict19@gmail.com>
2022-05-17 16:59:12 +0530

    intro section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 
 const Container = styled.div`
     height: 100vh;
+    overflow: hidden;
     
 `
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import women from '../img/women.jpg'
 
 const Container=styled.div`
 height: calc(100vh - 50px);
@@ -28,6 +29,48 @@ const Desc = styled.p`
     width: 100%;
   }
 `;
+const Info = styled.div`
+  width: 60%;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  cursor: pointer;
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
+`;
+
+const Contact =styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const Phone= styled.span`
+    color: #f0667d;
+`;
+
+const ContactText=styled.span`
+color: gray;
+`
+const Image = styled.img`
+    width: 100%;
+`
+
 
 const Intro = () => {
   return (
@@ -39,8 +82,16 @@ const Intro = () => {
           with our clients is the only way to have a real impact on their
           business.
         </Desc>
+        <Info>
+          <Button>START A PROJECT </Button>
+          <Contact>
+            <Phone>CALL US (091) 111-12111</Phone>
+            <ContactText>For any question or concern</ContactText>
+          </Contact>
+        </Info>
+
         </Left>
-        <Right>right</Right>
+        <Right><Image src={women}></Image></Right>
     </Container>
   )
 }
